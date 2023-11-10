58cfac4b2505fa448a36d2a016d7a934bc97a9eb
Livingstone-Billy <livingstoneochadia@gmail.com>
2023-11-10 08:45:12 +0300

    code refactoring

--- src/main/java/com/stacklink/inventory_management_system/DialogWindow.java
+++ src/main/java/com/stacklink/inventory_management_system/DialogWindow.java
@@ -15,7 +15,8 @@ import javafx.stage.StageStyle;
 
 public class DialogWindow {
     public void showDialog(String title, String message){
-        Stage window = new Stage(StageStyle.UTILITY);
+        Stage window;
+        window = new Stage(StageStyle.UTILITY);
 
         VBox root = new VBox(40);
         root.setPadding(new Insets(30.0,30.0,30.0,30.0));
@@ -44,41 +45,4 @@ public class DialogWindow {
         window.showAndWait();
     }
 
-    public void showAboutDialog(String title, String message){
-        Stage window = new Stage(StageStyle.UTILITY);
-
-
-        VBox root = new VBox(40);
-        root.setPadding(new Insets(30.0,30.0,30.0,30.0));
-        HBox pane = new HBox();
-
-        Label mMessage = new Label(message);
-        mMessage.setFont(Font.font("Verdana", FontWeight.MEDIUM, 16));
-        pane.getChildren().add(mMessage);
-
-        pane.setAlignment(Pos.CENTER);
-
-        HBox btnBox = new HBox(40);
-        Button accept = new Button("Accept");
-        accept.setTextFill(Color.WHITE);
-        accept.setFont(Font.font(18.0));
-        accept.setBackground(new Background(new BackgroundFill(Color.DODGERBLUE, CornerRadii.EMPTY, Insets.EMPTY)));
-        accept.setOnAction(e -> window.close());
-
-        Button decline = new Button("Decline");
-        decline.setTextFill(Color.WHITE);
-        decline.setFont(Font.font(18.0));
-        decline.setBackground(new Background(new BackgroundFill(Color.CRIMSON, CornerRadii.EMPTY, Insets.EMPTY)));
-        decline.setOnAction(e -> window.close());
-        btnBox.setAlignment(Pos.BOTTOM_CENTER);
-        btnBox.getChildren().addAll(accept, decline);
-
-        root.getChildren().addAll(pane, btnBox);
-        Scene scene = new Scene(root, 600, 160, true);
-        window.setScene(scene);
-        window.setTitle(title);
-        window.setResizable(false);
-        window.initModality(Modality.APPLICATION_MODAL);
-        window.showAndWait();
-    }
 }
